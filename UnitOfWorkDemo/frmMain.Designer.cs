a9521711ce9ff677b391c432d21972ae1b965b8d
petertop <peter.topolsek@gmail.com>
2015-11-23 09:20:08 +0100

    Change to production...

--- UnitOfWorkDemo/frmMain.Designer.cs
+++ UnitOfWorkDemo/frmMain.Designer.cs
@@ -30,6 +30,7 @@
         {
             this.button1 = new System.Windows.Forms.Button();
             this.cmdWorkWithRepository = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -52,11 +53,21 @@
             this.cmdWorkWithRepository.UseVisualStyleBackColor = true;
             this.cmdWorkWithRepository.Click += new System.EventHandler(this.cmdWorkWithRepository_Click);
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(31, 13);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 158);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.cmdWorkWithRepository);
             this.Controls.Add(this.button1);
             this.Name = "frmMain";
@@ -69,6 +80,7 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button cmdWorkWithRepository;
+        private System.Windows.Forms.Button button2;
     }
 }
 
